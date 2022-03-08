047033e7e7f71b55279b12f26b87ea7dbd540bcb
zhiyangSong <moruanqu@163.com>
2022-03-08 21:25:18 +0800

    add process to  get_data

--- BSpline/test.py
+++ BSpline/test.py
@@ -7,18 +7,18 @@ import matplotlib.pyplot as plt
 
 
 point = np.load("./position.npy")
-
+print(point)
 D_X = point[0]
 D_Y = point[1,:]
-point = bs.get_control_point(D_X , D_Y)
-print(point)
-np.save('./control_point' ,point )
+# point = bs.get_control_point(D_X , D_Y)
+# print(point)
+# np.save('./control_point' ,point )
 
 # plt.plot(D_X  ,D_Y)
 # plt.show()
 # print(D_X)
 
 
-# bs.curve_approx_figure(D_X , D_Y)
+bs.curve_approx_figure(D_X , D_Y)
 
 
