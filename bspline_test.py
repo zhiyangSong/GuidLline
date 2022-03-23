b5e93aefff90d28bb9233d63fe362734af6839e0
zhiyangSong <moruanqu@163.com>
2022-03-23 16:09:11 +0800

    change bspline_test

--- bspline_test.py
+++ bspline_test.py
@@ -7,36 +7,37 @@ from process_data.uniformization import uniformization
     
     
 """
-test for bspline
+test for bspline 两种方法
+
 从包中拿数据,通过b样条处理成 控制点坐标,并拟合成曲线
 """
 
 
-# bs = BS_curve(8,3)
+bs = BS_curve(8,3)
 
-# dataDir = './data/bag_20220211_1/'
-# tra = np.loadtxt("{}tra.csv".format(dataDir), delimiter=",", dtype="double")
-# point = tra[:, :2]
-# point = uniformization(point , 5)
+dataDir = './data/bag_20220211_1/'
+tra = np.loadtxt("{}tra.csv".format(dataDir), delimiter=",", dtype="double")
+point = tra[:, :2]
+point = uniformization(point , 5)
 
 
-# xx = point[: , 0]
-# yy = point[: , 1]
-# plt.scatter(xx ,yy)
+xx = point[: , 0]
+yy = point[: , 1]
+plt.scatter(xx ,yy)
 
-# paras = bs.estimate_parameters(point)
-# knots = bs.get_knots()
-# if bs.check():
-#     cp = bs.approximation(point)
-# uq = np.linspace(0,1,101)
-# y = bs.bs(uq)
+paras = bs.estimate_parameters(point)
+knots = bs.get_knots()
+if bs.check():
+    cp = bs.approximation(point)
+uq = np.linspace(0,1,101)
+y = bs.bs(uq)
 
-# plt.plot(y[:,0],y[:,1],'r')
-# plt.plot(cp[:,0],cp[:,1],'y')
-# plt.scatter(cp[:,0],cp[:,1],c = 'y')
-# print("cp.shape:{}".format(cp.shape))
-# print(cp)
-# plt.show()
+plt.plot(y[:,0],y[:,1],'r')
+plt.plot(cp[:,0],cp[:,1],'y')
+plt.scatter(cp[:,0],cp[:,1],c = 'y')
+print("cp.shape:{}".format(cp.shape))
+print(cp)
+plt.show()
 
 
 
@@ -44,8 +45,6 @@ test for bspline
 
 """
 通过控制点和节点出拟合曲线
-
-
 
 """
 bs = BS_curve(8,3)
