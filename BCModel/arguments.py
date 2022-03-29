713c449ccdf83cdb1af90882249a4a119aa8b92c
zhiyangSong <moruanqu@163.com>
2022-03-29 18:17:33 +0800

    add szy

--- BCModel/arguments.py
+++ BCModel/arguments.py
@@ -8,7 +8,7 @@ def get_common_args():
     parser.add_argument('--batch_size', type=int, default=6, help='the size of batch')
     parser.add_argument("--lr", type=float, default=5e-2, help="learning rate for adam optimizer")
 
-    parser.add_argument('--episodes_num', type=int, default=1500, help='the number of episodes')
+    parser.add_argument('--episodes_num', type=int, default=2000, help='the number of episodes')
 
 
     parser.add_argument('--save_interval', type=int, default=100, help='the number of steps to save the model')
@@ -20,7 +20,7 @@ def get_common_args():
     parser.add_argument('--max_steps', type=int, default=1000, help='the max number of step for a episode')
     parser.add_argument("--num_units_1", type=int, default=128, help="number of units in the mlp")
     parser.add_argument("--num_units_2", type=int, default=64, help="number of units in the mlp")
-    parser.add_argument("--input_size", type=int, default=23, help="input size")
+    parser.add_argument("--input_size", type=int, default=24, help="input size")
     parser.add_argument("--output_size", type=int, default=18, help="output size: B-spline contral points")
     parser.add_argument("--device", type=str, default="cpu", help="use CPU or GPU")
     parser.add_argument("--eva_period", type=int, default=5, help="the evaluate time")
